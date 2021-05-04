009862717e8e3b8f0c68c5b4fb0b9746ca3f512e
Robert4582 <43041342+Robert4582@users.noreply.github.com>
2021-05-04 10:18:18 +0200

    made animation class

---
+++ TestRogue/TestRogue/Animation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace TestRogue
+{
+    public class Animation : Component
+    {
+        SpriteBatch spriteBatch;
+
+        public override void Draw()
+        {
+            spriteBatch.Begin();
+
+            //Example of how to draw
+            //spriteBatch.Draw(sprite, position, color);
+
+            spriteBatch.End();
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(SpriteBatch spriteBatch)
+        {
+            
+        }
+    }
+}
