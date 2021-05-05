bcc5b9355d8478c16cd17401dc3ad2be7864ae7b
Robert4582 <43041342+Robert4582@users.noreply.github.com>
2021-05-05 12:06:48 +0200

    Early Combat class

---
+++ TestRogue/UnitTestProject/CombatTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestRogue;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class CombatTest
+    {
+
+        [TestMethod]
+        public void CanPlayerLossHP()
+        {
+
+            //Arrange
+            Combat battle = new Combat();
+            int playerDMG = 25;
+            int playerHP = 50;
+            int newHP = playerHP - playerDMG;
+
+            //Act
+            int hp = battle.DMGPlayer();
+
+            //Assert
+            Assert.AreEqual(hp, newHP);
+
+        }
+
+        [TestMethod]
+        public void CanDie()
+        {
+
+            //Arrange
+            Combat battle = new Combat();
+            bool isAlive = false;
+            
+
+            //Act
+            bool died = battle.PlayerDied();
+
+            //Assert
+            Assert.AreEqual(died, isAlive);
+
+        }
+    }
+}
